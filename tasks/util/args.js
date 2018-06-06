e3f65eeca67f40c093d326bb28c743aa0d05cfb8
zoouii <zoouii@163.com>
2018-06-06 14:04:36 +0800

    first commit

---
+++ tasks/util/args.js
@@ -0,0 +1,33 @@
+import yargs from 'yargs';
+
+const args = yargs
+
+    .option('prod',{
+        boolean: true,
+        default: false,
+        describe: 'min all jsfiles'
+    })
+
+    .option('watch',{
+        boolean: true,
+        default: false,
+        describe: 'watch all files'
+    })
+
+    .option('verbose',{
+        boolean: true,
+        default: false,
+        describe: 'all logs'
+    })
+
+    .option('sourcemaps',{
+        describe: 'force to create sourcemaps'
+    })
+
+    .option('port',{
+        string: true,
+        default: 8080,
+        describe: 'server port'
+    })
+
+    .argv
